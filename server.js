c3fa5f3266143038d0a7aba72fb9f1e181153db5
aylmerbritto <britto.lovable@gmail.com>
2016-10-30 14:11:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'homeattempt2.html'));
+  res.sendFile(path.join(__dirname, 'https://cloud.imad.hasura.io/code/files/homeattempt2.html'));
 });
 
 //app.get('/ui/style.css', function (req, res) {
