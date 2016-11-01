7c9cfb1a379ce9ca4c2292ca61df0d4e1cd5ea02
aylmerbritto <britto.lovable@gmail.com>
2016-11-01 11:24:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'homeattempt2.html'));
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 //app.get('/ui/style.css', function (req, res) {
